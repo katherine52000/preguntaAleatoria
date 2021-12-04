795bdd49b570a029d8cc7e305a333de23a7cd19f
katherine castrejon <kath52000@outlook.es>
2021-12-04 23:37:38 +0100

    practica

---
+++ index.js
@@ -0,0 +1,15 @@
+let si = document.getElementById('si');
+si.addEventListener("click",function(e){
+alert("eres mio papi <3");
+
+});
+
+
+
+let no=document.getElementById('no');
+no.addEventListener("mousemove", function(e){
+   let x = Math.round (Math.random()*93);
+   let y = Math.round (Math.random()*93);
+   no.style.left = x+"%";
+   no.style.top = y+"%";
+});
